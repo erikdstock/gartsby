48504fb87778aa3054c4bedb4f22d5e7d8f7be9f
Erik Stockmeier <erik.d.stock@gmail.com>
2019-06-05 16:44:52 -0400

    set up ts

---
+++ gatsby-browser.js
@@ -0,0 +1,4 @@
+import React from "react"
+import { Theme } from "@artsy/palette"
+
+export const wrapRootElement = ({ element }) => <Theme>{element}</Theme>
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { Box, Sans } from "@artsy/palette"
+
+export default () => (
+  <Box>
+    <Sans size="3t">Hello World!</Sans>
+  </Box>
+)
